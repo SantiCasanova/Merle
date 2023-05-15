428aec9310cae0b00dc981abe203ddff0f8b5f9c
Santiago Casanova <santiagocasanova@Santiagos-MacBook-Pro.local>
2023-05-15 14:06:51 +0200

    font changed

--- src/components/slogan/Slogan.styled.js
+++ src/components/slogan/Slogan.styled.js
@@ -1,5 +1,14 @@
 import styled from 'styled-components'
 
 export const SloganStyled = styled.span`
-  background-color: red;
+  width: 16.375rem;
+  height: auto;
+  font-family: 'balooPaaji', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 100%;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #ffffff;
 `
