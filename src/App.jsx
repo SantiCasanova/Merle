8341adb711aee026114684da8bba06b8a098427e
Santiago Casanova <santiagocasanova@Santiagos-MacBook-Pro.local>
2023-05-17 21:06:27 +0200

    Pack created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Home from './pages/home/Home'
+import Pack from './pages/pack/Pack'
 
 function App() {
   return (
     <>
       <Home />
+      <Pack />
     </>
   )
 }
---
+++ src/pages/pack/Pack.jsx
@@ -0,0 +1,7 @@
+import { PackStyled } from './Pack.styled'
+
+function Pack() {
+  return <PackStyled>PACKS</PackStyled>
+}
+
+export default Pack
